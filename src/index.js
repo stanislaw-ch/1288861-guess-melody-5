d36dba9e392760447844efda1a956fb39cf3fc05
Stanislaw Chirko <post.chirko@gmail.com>
2020-11-15 21:36:07 +0100

     добавляет выполнение задания 19. Учебный проект: истина где-то на сервере

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+import {createAPI} from "./services/api";
 import App from "./components/app/app";
-import {reducer} from "./store/reducer";
+import rootReducer from "./store/reducers/root-reducer";
+import {requireAuthorization} from "./store/action";
+import {fetchQuestionList, checkAuth} from "./store/api-actions";
+import {AuthorizationStatus} from "./const";
+
+const api = createAPI(
+    () => store.dispatch(requireAuthorization(AuthorizationStatus.NO_AUTH))
+);
 
 const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
+
+store.dispatch(fetchQuestionList());
+store.dispatch(checkAuth());
 
 ReactDOM.render(
     <Provider store={store}>
