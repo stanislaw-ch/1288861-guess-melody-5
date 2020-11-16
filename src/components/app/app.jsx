d12d50205b26a075414823ba9e9fe0fef9d8e174
Stanislaw Chirko <post.chirko@gmail.com>
2020-11-16 09:22:05 +0100

    добавляет выполнение задания 20. Учебный проект: добро пожаловать, или посторонним вход воспрещён

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import {Switch, Route, BrowserRouter} from "react-router-dom";
+import {Switch, Route, Router as BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
 import AuthScreen from "../auth-screen/auth-screen";
 import GameOverScreen from "../game-over-screen/game-over-screen";
 import WinScreen from "../win-screen/win-screen";
 import GameScreen from "../game-screen/game-screen";
+import PrivateRoute from "../private-route/private-route";
+import browserHistory from "../../browser-history";
 import {MAX_MISTAKE_COUNT} from "../../const";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter history={browserHistory}>
       <Switch>
         <Route exact
           path="/"
@@ -20,16 +22,24 @@ const App = () => {
             />
           )}
         />
-        <Route exact path="/login">
-          <AuthScreen />
-        </Route>
         <Route exact
-          path="/result"
+          path="/login"
           render={({history}) => (
-            <WinScreen
+            <AuthScreen
               onReplayButtonClick={() => history.push(`/game`)}
             />
           )}
+        />
+        <PrivateRoute
+          exact
+          path={`/result`}
+          render={({history}) => {
+            return (
+              <WinScreen
+                onReplayButtonClick={() => history.push(`/game`)}
+              />
+            );
+          }}
         />
         <Route exact
           path="/lose"
--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {Redirect} from 'react-router-dom';
 import {connect} from "react-redux";
 import {incrementStep, incrementMistake, resetGame} from "../../store/action";
-import {GameType, MAX_MISTAKE_COUNT} from '../../const';
+import {GameType, MAX_MISTAKE_COUNT, AppRoute} from '../../const';
 import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
 import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
 import Mistakes from "../mistakes/mistakes";
@@ -27,13 +27,13 @@ const GameScreen = (props) => {
 
   if (mistakes >= MAX_MISTAKE_COUNT) {
     return (
-      <Redirect to="/lose" />
+      <Redirect to={AppRoute.LOSE} />
     );
   }
 
   if (step >= questions.length || !question) {
     return (
-      <Redirect to="/result" />
+      <Redirect to={AppRoute.RESULT} />
     );
   }
 
@@ -41,6 +41,7 @@ const GameScreen = (props) => {
     case GameType.ARTIST:
       return (
         <ArtistQuestionScreenWrapped
+          key={step}
           question={question}
           onAnswer={onUserAnswer}
         >
@@ -50,6 +51,7 @@ const GameScreen = (props) => {
     case GameType.GENRE:
       return (
         <GenreQuestionScreenWrapped
+          key={step}
           question={question}
           onAnswer={onUserAnswer}
         >
@@ -58,7 +60,7 @@ const GameScreen = (props) => {
       );
   }
 
-  return <Redirect to="/" />;
+  return <Redirect to={AppRoute.ROOT} />;
 };
 
 GameScreen.propTypes = {
